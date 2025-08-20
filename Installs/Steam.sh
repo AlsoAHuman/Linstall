7c2927b2b3663af9e2ddc430834e27aa9fdb5314
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2025-08-20 17:11:19 +0000

    added steam.sh

---
+++ Installs/Steam.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo nala install apt-transport-https dirmngr ca-certificates curl -y
+curl -s http://repo.steampowered.com/steam/archive/stable/steam.gpg | sudo tee /usr/share/keyrings/steam.gpg > /dev/null
+echo deb [arch=amd64,i386 signed-by=/usr/share/keyrings/steam.gpg] http://repo.steampowered.com/steam/ stable steam | sudo tee /etc/apt/sources.list.d/steam.list
+sudo apt update
+sudo apt install libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 steam-launcher -y
+curl -fsSL "https://raw.githubusercontent.com/SteamClientHomebrew/Millennium/main/scripts/install.sh" | sudo sh -y
+sudo millennium patch
